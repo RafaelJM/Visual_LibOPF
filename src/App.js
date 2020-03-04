a49deb33305cd0996195fdc1f8323a0cef72b8d2
Rafael <rfel.jm@gmail.com>
2020-03-03 21:54:53 -0300

    learning react and grid layout
    https://github.com/STRML/react-grid-layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
+import GridLayout from 'react-grid-layout';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyFirstGrid></MyFirstGrid>
     </div>
   );
+}
+
+class MyFirstGrid extends React.Component {
+  render() {
+    // layout is an array of objects, see the demo for more complete usage
+    const layout = [
+      {i: 'ToolBar', x: 0, y: 0, w: 1, h: 2, static: true},
+      {i: 'Info', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+      {i: 'Objects', x: 4, y: 0, w: 1, h: 2},
+      {i: 'Grid', x: 4, y: 0, w: 1, h: 2},
+      {i: 'Functions', x: 4, y: 0, w: 1, h: 2}
+    ];
+    return (
+      <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+        <div key="ToolBar">a</div>
+        <div key="Info">b</div>
+        <div key="Objects">c</div>
+        <div key="Grid">c</div>
+        <div key="Functions">c</div>
+      </GridLayout>
+    )
+  }
+}
+
+class Info extends React.Component {
+  render() {
+    return (<p></p>);
+  }
+}
+
+class Objects extends React.Component {
+  render() {
+    return (<p></p>);
+  }
+}
+
+class Function extends React.Component {
+  render() {
+    return (<p></p>);
+  }
+}
+
+class Grid extends React.Component {
+  render() {
+    return (<p></p>);
+  }
+}
+
+class ToolBar extends React.Component {
+  render() {
+    return (<p></p>);
+  }
 }
 
 export default App;
