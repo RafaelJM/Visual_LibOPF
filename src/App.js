65d17494697d5c1504d3aec7dc6b5f33a1081944
Rafael <rfel.jm@gmail.com>
2020-03-09 01:38:02 -0300

    Trying to use a graph viewer
    react-d3-graph

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import GridLayout from 'react-grid-layout';
 import './App.css';
+import { Graph } from "react-d3-graph";
 
 function App() {
   return (
@@ -27,7 +28,7 @@ class MyFirstGrid extends React.Component {
         <div key="ToolBar">a</div>
         <div key="Info">b</div>
         <div key="Objects">c</div>
-        <div key="Grid">c</div>
+        <div key="Grid"></div>
         <div key="Functions">c</div>
       </GridLayout>
     )
@@ -53,8 +54,75 @@ class Function extends React.Component {
 }
 
 class Grid extends React.Component {
+  constructor(props){
+    var config = {
+      "automaticRearrangeAfterDropNode": false,
+      "collapsible": false,
+      "directed": false,
+      "focusAnimationDuration": 0.75,
+      "focusZoom": 1,
+      "height": 400,
+      "highlightDegree": 1,
+      "highlightOpacity": 1,
+      "linkHighlightBehavior": true,
+      "maxZoom": 8,
+      "minZoom": 0.1,
+      "nodeHighlightBehavior": true,
+      "panAndZoom": false,
+      "staticGraph": false,
+      "staticGraphWithDragAndDrop": true,
+      "width": 800,
+      "d3": {
+        "alphaTarget": 0.05,
+        "gravity": -400,
+        "linkLength": 180,
+        "linkStrength": 1,
+        "disableLinkForce": true
+      },
+      "node": {
+        "color": "#d3d3d3",
+        "fontColor": "black",
+        "fontSize": 12,
+        "fontWeight": "normal",
+        "highlightColor": "SAME",
+        "highlightFontSize": 12,
+        "highlightFontWeight": "bold",
+        "highlightStrokeColor": "blue",
+        "highlightStrokeWidth": "SAME",
+        "labelProperty": "name",
+        "mouseCursor": "pointer",
+        "opacity": 1,
+        "renderLabel": true,
+        "size": 500,
+        "strokeColor": "none",
+        "strokeWidth": 2,
+        "svg": "",
+        "symbolType": "circle"
+      },
+      "link": {
+        "color": "#d3d3d3",
+        "fontColor": "black",
+        "fontSize": 12,
+        "fontWeight": "normal",
+        "highlightColor": "blue",
+        "highlightFontSize": 8,
+        "highlightFontWeight": "bold",
+        "labelProperty": "label",
+        "mouseCursor": "pointer",
+        "opacity": 1,
+        "renderLabel": true,
+        "semanticStrokeWidth": true,
+        "strokeWidth": 1.5,
+        "markerHeight": 6,
+        "markerWidth": 6
+      }
+    }
+  }
+
   render() {
-    return (<p></p>);
+    return (<div>
+      <Graph id="graph" config={this.config} data={this.subGraph}></Graph>
+    </div>);
   }
 }
 
@@ -72,34 +140,43 @@ class Teste extends React.Component {
   
   click() {
     var fileInput = document.getElementById("dat");
-
     var files = fileInput.files;
     var reader = new FileReader();
+
     reader.readAsArrayBuffer(files[0]);
+
+    var subGraph = {
+        nodes: [     ],
+        links: [     ]
+    };
 
     reader.onload = function() {
       var dv = new DataView(reader.result);
-      console.log(dv);
       var cont = 0;
-      var nnodes = dv.getInt32(0,true);//nnodes
-      dv.getInt32(cont=cont+4,true);//nlabels
+      var nnodes = dv.getInt32(cont,true);//nnodes
+      var nlabels = dv.getInt32(cont=cont+4,true);//nlabels
       var nfeats = dv.getInt32(cont=cont+4,true);//nfeats
       for(var i = 0; i < nnodes; i++){
-        dv.getInt32(cont=cont+4,true);//position
-        dv.getInt32(cont=cont+4,true);//truelabel
+        subGraph.nodes[i] = {id: dv.getInt32(cont=cont+4,true),//position
+        truelabel: dv.getInt32(cont=cont+4,true),//truelabel
+        feats: [  ]};
         for(var j = 0; j < nfeats; j++){
-          dv.getFloat32(cont=cont+4,true);//feat
+          subGraph.nodes[i].feats[j] = dv.getFloat32(cont=cont+4,true);//feat
         }
       }
+      console.log(subGraph);
     };
+
     reader.onerror = function() {
       console.log(reader.error);
     };
   }
 
   render() {
-    return (<div><input type="file" id="dat" multiple></input>
-    <button onClick={this.click} id ="button">arroz</button></div>);
+    return (<div>
+      <input type="file" id="dat" multiple></input>
+      <button onClick={this.click} id ="button">arroz</button>
+    </div>);
   }
 }
 
