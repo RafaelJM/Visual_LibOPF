791df67d72ab7f97c5a26acc518e0c9c14268d0e
Rafael <rfel.jm@gmail.com>
2020-03-18 21:35:30 -0300

    Trying to use sigmajs to show the Graph
    The react-d3-graph didn't work very well

--- src/App.js
+++ src/App.js
@@ -2,155 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import GridLayout from 'react-grid-layout';
 import './App.css';
-import { Graph } from "react-d3-graph";
+import * as d3 from 'd3';
+import {Sigma, RandomizeNodePositions, RelativeSize, NodeShapes } from 'react-sigma';
 
 function App() {
   return (
     <div className="App">
       <MyFirstGrid></MyFirstGrid>
-      <Teste></Teste>
     </div>
   );
 }
 
 class MyFirstGrid extends React.Component {
-  render() {
-    // layout is an array of objects, see the demo for more complete usage
-    const layout = [
-      {i: 'ToolBar', x: 0, y: 0, w: 1, h: 2, static: true},
-      {i: 'Info', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
-      {i: 'Objects', x: 4, y: 0, w: 1, h: 2},
-      {i: 'Grid', x: 4, y: 0, w: 1, h: 2},
-      {i: 'Functions', x: 4, y: 0, w: 1, h: 2}
-    ];
-    return (
-      <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-        <div key="ToolBar">a</div>
-        <div key="Info">b</div>
-        <div key="Objects">c</div>
-        <div key="Grid"></div>
-        <div key="Functions">c</div>
-      </GridLayout>
-    )
-  }
-}
-
-class Info extends React.Component {
-  render() {
-    return (<p></p>);
-  }
-}
-
-class Objects extends React.Component {
-  render() {
-    return (<p></p>);
-  }
-}
-
-class Function extends React.Component {
-  render() {
-    return (<p></p>);
-  }
-}
-
-class Grid extends React.Component {
-  constructor(props){
-    var config = {
-      "automaticRearrangeAfterDropNode": false,
-      "collapsible": false,
-      "directed": false,
-      "focusAnimationDuration": 0.75,
-      "focusZoom": 1,
-      "height": 400,
-      "highlightDegree": 1,
-      "highlightOpacity": 1,
-      "linkHighlightBehavior": true,
-      "maxZoom": 8,
-      "minZoom": 0.1,
-      "nodeHighlightBehavior": true,
-      "panAndZoom": false,
-      "staticGraph": false,
-      "staticGraphWithDragAndDrop": true,
-      "width": 800,
-      "d3": {
-        "alphaTarget": 0.05,
-        "gravity": -400,
-        "linkLength": 180,
-        "linkStrength": 1,
-        "disableLinkForce": true
-      },
-      "node": {
-        "color": "#d3d3d3",
-        "fontColor": "black",
-        "fontSize": 12,
-        "fontWeight": "normal",
-        "highlightColor": "SAME",
-        "highlightFontSize": 12,
-        "highlightFontWeight": "bold",
-        "highlightStrokeColor": "blue",
-        "highlightStrokeWidth": "SAME",
-        "labelProperty": "name",
-        "mouseCursor": "pointer",
-        "opacity": 1,
-        "renderLabel": true,
-        "size": 500,
-        "strokeColor": "none",
-        "strokeWidth": 2,
-        "svg": "",
-        "symbolType": "circle"
-      },
-      "link": {
-        "color": "#d3d3d3",
-        "fontColor": "black",
-        "fontSize": 12,
-        "fontWeight": "normal",
-        "highlightColor": "blue",
-        "highlightFontSize": 8,
-        "highlightFontWeight": "bold",
-        "labelProperty": "label",
-        "mouseCursor": "pointer",
-        "opacity": 1,
-        "renderLabel": true,
-        "semanticStrokeWidth": true,
-        "strokeWidth": 1.5,
-        "markerHeight": 6,
-        "markerWidth": 6
-      }
-    }
-  }
-
-  render() {
-    return (<div>
-      <Graph id="graph" config={this.config} data={this.subGraph}></Graph>
-    </div>);
-  }
-}
-
-class ToolBar extends React.Component {
-  render() {
-    return (<p></p>);
-  }
-}
-
-class Teste extends React.Component {
   constructor(props) {
     super(props);
     this.click = this.click.bind(this);
+    this.state = {
+        data: {nodes:[{id:"n1", x: 0.399, y: 0.399}, {id:"n2", x: 0.4, y: 0.4}], edges:[]},
+    };
   }
-  
+
   click() {
     var fileInput = document.getElementById("dat");
     var files = fileInput.files;
     var reader = new FileReader();
+    const scope = this;
 
     reader.readAsArrayBuffer(files[0]);
 
-    var subGraph = {
-        nodes: [     ],
-        links: [     ]
-    };
-
     reader.onload = function() {
+      var subGraph= {
+        nodes: [],
+        edges:[],
+      };
       var dv = new DataView(reader.result);
       var cont = 0;
       var nnodes = dv.getInt32(cont,true);//nnodes
@@ -159,25 +43,63 @@ class Teste extends React.Component {
       for(var i = 0; i < nnodes; i++){
         subGraph.nodes[i] = {id: dv.getInt32(cont=cont+4,true),//position
         truelabel: dv.getInt32(cont=cont+4,true),//truelabel
-        feats: [  ]};
+        feats: [  ], x:0, y:0};
         for(var j = 0; j < nfeats; j++){
           subGraph.nodes[i].feats[j] = dv.getFloat32(cont=cont+4,true);//feat
         }
+        subGraph.nodes[i].x = subGraph.nodes[i].feats[0];
+        subGraph.nodes[i].y = subGraph.nodes[i].feats[1];
       }
-      console.log(subGraph);
-    };
-
-    reader.onerror = function() {
-      console.log(reader.error);
+      scope.setState({ data:subGraph });
+      console.log(scope.state);
     };
   }
 
   render() {
-    return (<div>
-      <input type="file" id="dat" multiple></input>
-      <button onClick={this.click} id ="button">arroz</button>
-    </div>);
+    const layout = [
+      {i: 'ToolBar', x: 0, y: 0, w: 1, h: 2, static: true},
+      {i: 'Info', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4, static: true},
+      {i: 'Objects', x: 4, y: 0, w: 1, h: 2, static: true},
+      {i: 'Grid', x: 0, y: 0, w: 10, h: 10, static: true},
+      {i: 'Functions', x: 4, y: 0, w: 1, h: 2}
+    ];
+
+    return (
+      <div>
+        <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+          <div key="ToolBar">a</div>
+          <div key="Info">b</div>
+          <div key="Objects">c</div>
+          <div key="Grid"><Graph data={this.state.data}/></div>
+          <div key="Functions">d</div>
+        </GridLayout>
+        <div>
+          <input type="file" id="dat" multiple></input>
+          <button onClick={this.click} id ="button">load</button>
+        </div>
+      </div>
+    )
   }
 }
+
+class Graph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.click = this.click.bind(this);
+  }
+
+  click() {console.log(this.props.data);}
+
+  render() {
+    return(<div><button onClick={this.click} id ="button">show</button>
+
+    <Sigma graph={this.props.data} settings={{drawEdges:true}}>
+      <RelativeSize initialSize={15}/>
+    </Sigma>
+  
+  </div>)
+  }
+}
+
 
 export default App;
